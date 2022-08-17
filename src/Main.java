7283f625d7cda62837e9be5fac8e3b7c4cf0cfd3
OscarAlvarezV <oscar.alvarez.vi@gmaqil.com>
2022-08-16 21:09:02 -0500

    Mi primer commit by Oscar Alvarez

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,10 @@ public class Main {
         CommitCamilo camilo = new CommitCamilo("Juan Camilo Silva Zabala", 20, "Estudiante MINTIC 2022");
         camilo.mensajeBienvenida();
 
+        CommitOscar oscarAlvarez = new CommitOscar("",0,"",0);
+        oscarAlvarez.setDireccion("avenida siempre viva 742");
+        oscarAlvarez.setMetros(89);
+        oscarAlvarez.setHabitaciones(4);
+        oscarAlvarez.setPropietario("homero simpson");
     }
 }
