1dd57d26e43a8349d81b1dd20d326cf5aae348a9
dianisr2 <dianis3008@gmail.com>
2022-08-16 22:28:09 -0500

    Mi primer commit by Diana Rodriguez R

--- src/Main.java
+++ src/Main.java
@@ -4,11 +4,16 @@ public class Main {
         CommitCamilo camilo = new CommitCamilo("Juan Camilo Silva Zabala", 20, "Estudiante MINTIC 2022");
         camilo.mensajeBienvenida();
 
+        Carro Diana = new Carro("Pichirilo", "ABC123", "Kia");
+        System.out.println(Diana.mensajeBievenida());
+
         CommitOscar oscarAlvarez = new CommitOscar("",0,"",0);
         oscarAlvarez.setDireccion("avenida siempre viva 742");
         oscarAlvarez.setMetros(89);
         oscarAlvarez.setHabitaciones(4);
         oscarAlvarez.setPropietario("homero simpson");
         oscarAlvarez.mensajeCommitOscar();
+
+
     }
 }
