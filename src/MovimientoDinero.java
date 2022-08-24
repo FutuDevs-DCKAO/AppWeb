f9959567cf22b4486ebbe1bd31cf982b08c93f77
kikejaimes <kikejaimes0705@gmail.com>
2022-08-24 17:15:56 -0500

    Creando clase MovimientoDinero

---
+++ src/MovimientoDinero.java
@@ -0,0 +1,56 @@
+public class MovimientoDinero {
+
+    private Double montoMovimiento;
+    private Double montosPositivos;
+    private Double montosNegativos;
+    private String conceptoMovimiento;
+    private String usuario;
+
+    public MovimientoDinero(Double montoMovimiento, Double montosPositivos, Double montosNegativos, String conceptoMovimiento, String usuario) {
+        this.montoMovimiento = montoMovimiento;
+        this.montosPositivos = montosPositivos;
+        this.montosNegativos = montosNegativos;
+        this.conceptoMovimiento = conceptoMovimiento;
+        this.usuario = usuario;
+    }
+
+    public Double getMontoMovimiento() {
+        return montoMovimiento;
+    }
+
+    public void setMontoMovimiento(Double montoMovimiento) {
+        this.montoMovimiento = montoMovimiento;
+    }
+
+    public Double getMontosPositivos() {
+        return montosPositivos;
+    }
+
+    public void setMontosPositivos(Double montosPositivos) {
+        this.montosPositivos = montosPositivos;
+    }
+
+    public Double getMontosNegativos() {
+        return montosNegativos;
+    }
+
+    public void setMontosNegativos(Double montosNegativos) {
+        this.montosNegativos = montosNegativos;
+    }
+
+    public String getConceptoMovimiento() {
+        return conceptoMovimiento;
+    }
+
+    public void setConceptoMovimiento(String conceptoMovimiento) {
+        this.conceptoMovimiento = conceptoMovimiento;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+}
