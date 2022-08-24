da005bfadd3161720eb14d951e02475b9d6abd5e
CamiloSilva21 <juancamilosilva1@gmail.com>
2022-08-24 13:43:09 -0500

    Creación clase Empleado V1

---
+++ src/Empleado.java
@@ -0,0 +1,10 @@
+public class Empleado {
+    private String nombre, correo, empresaPerteneciente, rol;
+
+    public Empleado(String nombre, String correo, String empresaPerteneciente, String rol){
+        this.nombre = nombre;
+        this.correo = correo;
+        this.empresaPerteneciente = empresaPerteneciente;
+
+    }
+}
